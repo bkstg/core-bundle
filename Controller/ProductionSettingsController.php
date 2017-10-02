ee2a8773ff9e1ae1d69a22df632b4a14438e35b7
Luke Bainbridge <luke.bainbridge@gmail.com>
2017-10-02 14:23:39 -0400

    Adding form handling and redirect.

--- Controller/ProductionSettingsController.php
+++ Controller/ProductionSettingsController.php
@@ -5,6 +5,8 @@ namespace Bkstg\CoreBundle\Controller;
 use Bkstg\CoreBundle\Entity\Production;
 use Bkstg\CoreBundle\Form\Type\ProductionSettingsType;
 use Bkstg\CoreBundle\Form\Type\ProductionType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
@@ -14,6 +16,7 @@ class ProductionSettingsController extends Controller
 {
     public function generalAction(
         $production_slug,
+        Request $request,
         AuthorizationCheckerInterface $auth
     ) {
         if (null === $production = $this->em->getRepository(Production::class)->findOneBy(['slug' => $production_slug])) {
@@ -24,9 +27,23 @@ class ProductionSettingsController extends Controller
         }
 
         $form = $this->form->create(ProductionSettingsType::class, $production);
+        $form->handleRequest($request);
+
         if ($form->isValid() && $form->isSubmitted()) {
             $this->em->persist($production);
             $this->em->flush();
+
+            // Set success message.
+            $this->session->getFlashBag()->add(
+                'success',
+                $this->translator->trans('Production "%production%" saved.', [
+                    '%production%' => $production->getName(),
+                ])
+            );
+
+            return new RedirectResponse($this->url_generator->generate('bkstg_production_settings_general', [
+                'production_slug' => $production->getSlug(),
+            ]));
         }
 
         return new Response($this->templating->render('@BkstgCore/Production/settings-general.html.twig', [
