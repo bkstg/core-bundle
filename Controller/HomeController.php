707a8e2e0abf0d4d61bab4b42ca1d457fa052145
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-06-10 14:50:17 -0400

    Creating home display.

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace Bkstg\CoreBundle\Controller;
 
+use Bkstg\CoreBundle\Entity\Production;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class HomeController extends Controller
 {
-    public function homeAction()
+    public function homeAction(TokenStorageInterface $token_storage)
     {
-        return new Response($this->templating->render('@BkstgCore/Home/home.html.twig'));
+        $user = $token_storage->getToken()->getUser();
+        $production_repo = $this->em->getRepository(Production::class);
+        $productions = $production_repo->findAll();
+        return new Response($this->templating->render('@BkstgCore/Home/home.html.twig', [
+            'productions' => $productions,
+        ]));
     }
 }
