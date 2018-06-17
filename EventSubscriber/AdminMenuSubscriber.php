1f87a4b29af808a4cb3c3ea23c1178a697d5fc8b
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-06-17 12:50:49 -0400

    Simplifying admin menu.

--- EventSubscriber/AdminMenuSubscriber.php
+++ EventSubscriber/AdminMenuSubscriber.php
@@ -3,30 +3,17 @@
 namespace Bkstg\CoreBundle\EventSubscriber;
 
 use Bkstg\CoreBundle\BkstgCoreBundle;
-use Bkstg\CoreBundle\Entity\ProductionMembership;
 use Bkstg\CoreBundle\Event\AdminMenuCollectionEvent;
 use Bkstg\CoreBundle\Event\MenuCollectionEvent;
-use Bkstg\CoreBundle\Menu\Item\IconMenuItem;
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Translation\TranslatorInterface;
 
 class AdminMenuSubscriber implements EventSubscriberInterface
 {
-
     private $factory;
-    private $url_generator;
-    private $translator;
 
-    public function __construct(
-        FactoryInterface $factory,
-        UrlGeneratorInterface $url_generator,
-        TranslatorInterface $translator
-    ) {
+    public function __construct(FactoryInterface $factory) {
         $this->factory = $factory;
-        $this->url_generator = $url_generator;
-        $this->translator = $translator;
     }
 
     public static function getSubscribedEvents()
@@ -45,10 +32,12 @@ class AdminMenuSubscriber implements EventSubscriberInterface
         $menu = $event->getMenu();
 
         // Create overview menu item.
-        $dashboard = $this->factory->createItem('dashboard', [
-            'label' => $this->translator->trans('menu_item.dashboard', [], BkstgCoreBundle::TRANSLATION_DOMAIN),
-            'uri' => $this->url_generator->generate('bkstg_admin_dashboard'),
-            'extras' => ['icon' => 'dashboard'],
+        $dashboard = $this->factory->createItem('menu_item.dashboard', [
+            'route' => 'bkstg_admin_dashboard',
+            'extras' => [
+                'icon' => 'dashboard',
+                'translation_domain' => BkstgCoreBundle::TRANSLATION_DOMAIN,
+            ],
         ]);
         $menu->addChild($dashboard);
     }
@@ -58,10 +47,12 @@ class AdminMenuSubscriber implements EventSubscriberInterface
         $menu = $event->getMenu();
 
         // Create productions menu item.
-        $productions = $this->factory->createItem('productions', [
-            'label' => $this->translator->trans('menu_item.productions', [], BkstgCoreBundle::TRANSLATION_DOMAIN),
-            'uri' => $this->url_generator->generate('bkstg_production_admin_list'),
-            'extras' => ['icon' => 'list'],
+        $productions = $this->factory->createItem('menu_item.productions', [
+            'route' => 'bkstg_production_admin_list',
+            'extras' => [
+                'icon' => 'list',
+                'translation_domain' => BkstgCoreBundle::TRANSLATION_DOMAIN,
+            ],
         ]);
         $menu->addChild($productions);
     }
