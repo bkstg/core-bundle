8ad90f46214fbc9524d42db9d8d894913267fbd3
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-06-17 12:44:48 -0400

    Menu overhaul continues.

--- EventSubscriber/MainMenuSubscriber.php
+++ EventSubscriber/MainMenuSubscriber.php
@@ -6,41 +6,31 @@ use Bkstg\CoreBundle\BkstgCoreBundle;
 use Bkstg\CoreBundle\Entity\Production;
 use Bkstg\CoreBundle\Event\MainMenuCollectionEvent;
 use Bkstg\CoreBundle\Event\MenuCollectionEvent;
-use Bkstg\CoreBundle\Menu\Item\IconMenuItem;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Menu\FactoryInterface;
 use MidnightLuke\GroupSecurityBundle\Model\GroupMemberInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Component\Translation\TranslatorInterface;
 
 class MainMenuSubscriber implements EventSubscriberInterface
 {
 
     private $factory;
-    private $url_generator;
     private $em;
     private $token_storage;
     private $auth;
-    private $translator;
 
     public function __construct(
         FactoryInterface $factory,
-        UrlGeneratorInterface $url_generator,
         EntityManagerInterface $em,
         TokenStorageInterface $token_storage,
-        AuthorizationCheckerInterface $auth,
-        TranslatorInterface $translator
+        AuthorizationCheckerInterface $auth
     ) {
         $this->factory = $factory;
-        $this->url_generator = $url_generator;
         $this->em = $em;
         $this->token_storage = $token_storage;
         $this->auth = $auth;
-        $this->translator = $translator;
     }
 
     public static function getSubscribedEvents()
@@ -48,7 +38,6 @@ class MainMenuSubscriber implements EventSubscriberInterface
         // return the subscribed events, their methods and priorities
         return [
            MainMenuCollectionEvent::NAME => [
-               ['addHomeMenuItem', 50],
                ['addProductionMenuItems', 25],
                ['addAdminMenuItem', -25],
                ['addLogoutMenuItem', -50],
@@ -56,18 +45,17 @@ class MainMenuSubscriber implements EventSubscriberInterface
         ];
     }
 
-    public function addHomeMenuItem(MenuCollectionEvent $event)
-    {
-    }
-
     public function addLogoutMenuItem(MenuCollectionEvent $event)
     {
         $menu = $event->getMenu();
 
-        $logout = $this->factory->createItem('logout', [
-            'label' => $this->translator->trans('menu_item.logout', [], BkstgCoreBundle::TRANSLATION_DOMAIN),
-            'uri' => $this->url_generator->generate('fos_user_security_logout'),
-            'extras' => ['icon' => 'sign-out'],
+        // Create logout menu item.
+        $logout = $this->factory->createItem('menu_item.logout', [
+            'route' => 'fos_user_security_logout',
+            'extras' => [
+                'icon' => 'sign-out',
+                'translation_domain' => BkstgCoreBundle::TRANSLATION_DOMAIN,
+            ],
         ]);
         $menu->addChild($logout);
     }
@@ -80,11 +68,13 @@ class MainMenuSubscriber implements EventSubscriberInterface
 
         $menu = $event->getMenu();
 
-        // Create overview menu item.
-        $admin = $this->factory->createItem('admin', [
-            'label' => $this->translator->trans('menu_item.admin', [], BkstgCoreBundle::TRANSLATION_DOMAIN),
-            'uri' => $this->url_generator->generate('bkstg_admin_redirect'),
-            'extras' => ['icon' => 'wrench'],
+        // Create admin menu item.
+        $admin = $this->factory->createItem('menu_item.admin', [
+            'route' => 'bkstg_admin_redirect',
+            'extras' => [
+                'icon' => 'wrench',
+                'translation_domain' => BkstgCoreBundle::TRANSLATION_DOMAIN,
+            ],
         ]);
         $menu->addChild($admin);
     }
@@ -98,7 +88,10 @@ class MainMenuSubscriber implements EventSubscriberInterface
         }
 
         // Create productions menu dropdown item.
-        $production_item = $this->factory->createItem('Productions');
+        $productions = $this->factory->createItem('menu_item.productions', [
+            'extras' => ['translation_domain' => BkstgCoreBundle::TRANSLATION_DOMAIN],
+        ]);
+
         $items = [];
         foreach ($user->getMemberships() as $membership) {
             // Get the group for this membership, if not a production skip out.
@@ -111,10 +104,8 @@ class MainMenuSubscriber implements EventSubscriberInterface
                 && !$group->isExpired()) {
                 // The membership is good, add a menu item.
                 $items[] = $this->factory->createItem($membership->getGroup()->getName(), [
-                    'uri' => $this->url_generator->generate(
-                        'bkstg_production_show',
-                        ['production_slug' => $membership->getGroup()->getSlug()]
-                    ),
+                    'route' => 'bkstg_production_show',
+                    'routeParameters' => ['production_slug' => $membership->getGroup()->getSlug()],
                     'extras' => ['translation_domain' => false],
                 ]);
             }
@@ -124,22 +115,10 @@ class MainMenuSubscriber implements EventSubscriberInterface
         usort($items, function ($a, $b) {
             return strcasecmp($a->getLabel(), $b->getLabel());
         });
-        $production_item->setChildren($items);
-
-        // Add production admin.
-        if ($this->auth->isGranted('ROLE_ADMIN')) {
-            $production_item->addChild($this->factory->createItem('<admin-separator>', [
-                'extras' => ['translation_domain' => false, 'separator' => true],
-            ]));
-            $production_item->addChild($this->factory->createItem('all_productions', [
-                'label' => $this->translator->trans('menu_item.all_productions', [], BkstgCoreBundle::TRANSLATION_DOMAIN),
-                'uri' => $this->url_generator->generate('bkstg_production_admin_list'),
-                'extras' => ['icon' => 'list'],
-            ]));
-        }
+        $productions->setChildren($items);
 
         // Add the production menu item to the main menu.
         $menu = $event->getMenu();
-        $menu->addChild($production_item);
+        $menu->addChild($productions);
     }
 }
