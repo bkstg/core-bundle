310c68e0cad7c1ccc264bae3fcb0401b0a731295
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-06-21 07:57:51 -0400

    Renaming production overview template, comments.

--- Controller/ProductionController.php
+++ Controller/ProductionController.php
@@ -9,15 +9,21 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-/**
- * Class for handling production CRUD operations.
- */
 class ProductionController extends Controller
 {
+    /**
+     * Read a production, simply redirects to the overview.
+     *
+     * @param  string                        $production_slug The production slug.
+     * @param  AuthorizationCheckerInterface $auth            The authorization checker service.
+     * @throws NotFoundHttpException                          When the production is not found.
+     * @throws AccessDeniedException                          When the user doesn't have access to the production.
+     * @return Response                                       The redirect response.
+     */
     public function readAction(
-        $production_slug,
+        string $production_slug,
         AuthorizationCheckerInterface $auth
-    ) {
+    ): RedirectResponse {
         // Lookup the production by production_slug.
         $production_repo = $this->em->getRepository(Production::class);
         if (null === $production = $production_repo->findOneBy(['slug' => $production_slug])) {
@@ -29,13 +35,23 @@ class ProductionController extends Controller
             throw new AccessDeniedException();
         }
 
+        // Redirect to the production overview.
         return new RedirectResponse($this->url_generator->generate('bkstg_production_overview', ['production_slug' => $production_slug]));
     }
 
+    /**
+     * Shows the production image and description, with block for timeline.
+     *
+     * @param  string                        $production_slug The production slug.
+     * @param  AuthorizationCheckerInterface $auth            The authorization checker service.
+     * @throws NotFoundHttpException                          When the production is not found.
+     * @throws AccessDeniedException                          When the user doesn't have access to the production.
+     * @return Response                                       The rendered response.
+     */
     public function overviewAction(
-        $production_slug,
+        string $production_slug,
         AuthorizationCheckerInterface $auth
-    ) {
+    ): Response {
         // Lookup the production by production_slug.
         $production_repo = $this->em->getRepository(Production::class);
         if (null === $production = $production_repo->findOneBy(['slug' => $production_slug])) {
@@ -48,7 +64,7 @@ class ProductionController extends Controller
         }
 
         // Return response.
-        return new Response($this->templating->render('@BkstgCore/Production/show.html.twig', [
+        return new Response($this->templating->render('@BkstgCore/Production/overview.html.twig', [
             'production' => $production,
         ]));
     }
