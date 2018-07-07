1eecc1cc74e5e551b8588fc87762354fe5d16929
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-07-07 07:38:44 -0400

    Publishable listener should work better now.

--- EventListener/PublishableListener.php
+++ EventListener/PublishableListener.php
@@ -31,7 +31,7 @@ class PublishableListener
     }
 
     /**
-     * Act before persisting an object.
+     * If an object is active set published value.
      *
      * @param LifecycleEventArgs $args The event arguments.
      */
@@ -46,10 +46,28 @@ class PublishableListener
         // Publish active objects immediately.
         if ($object->isActive()) {
             $object->setPublished(true);
-            $event = new EntityPublishedEvent($object);
-            $this->event_dispatcher->dispatch(EntityPublishedEvent::NAME, $event);
         } else {
             $object->setPublished(false);
+        }
+    }
+
+    /**
+     * If an object is added as published fire published event.
+     *
+     * @param LifecycleEventArgs $args The lifecycle arguments.
+     */
+    public function postPersist(LifecycleEventArgs $args): void
+    {
+        // Get the object and make sure it is what we need.
+        $object = $args->getObject();
+        if (!$object instanceof PublishableInterface) {
+            return;
+        }
+
+        // Execute published event after the object is persisted.
+        if ($object->isPublished()) {
+            $event = new EntityPublishedEvent($object);
+            $this->event_dispatcher->dispatch(EntityPublishedEvent::NAME, $event);
         }
     }
 
@@ -69,6 +87,30 @@ class PublishableListener
         // Active unpublished objects should be published.
         if ($object->isActive() && !$object->isPublished()) {
             $object->setPublished(true);
+        }
+    }
+
+    /**
+     * If an object is updated and published fire published event.
+     *
+     * @param LifecycleEventArgs $args The lifecycle arguments.
+     */
+    public function postUpdate(LifecycleEventArgs $args): void
+    {
+        // Get the object and make sure it is what we need.
+        $object = $args->getObject();
+        if (!$object instanceof PublishableInterface) {
+            return;
+        }
+
+        // Get the changeset from the unit of work.
+        $uow = $args->getObjectManager()->getUnitOfWork();
+        $changeset = $uow->getEntityChangeset($object);
+
+        // If the published state changed fire the published event.
+        if (isset($changeset['published'])
+            && false === $changeset['published'][0]
+            && true === $changeset['published'][1]) {
             $event = new EntityPublishedEvent($object);
             $this->event_dispatcher->dispatch(EntityPublishedEvent::NAME, $event);
         }
