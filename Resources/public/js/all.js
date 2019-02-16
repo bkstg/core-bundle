6a69dcfcec278039bddcc74cdac9b3f4097a3ab9
Luke Bainbridge <luke.bainbridge@gmail.com>
2019-02-16 11:24:36 -0500

    JS and CSS standard includes.

--- Resources/public/js/all.js
+++ Resources/public/js/all.js
@@ -1,15 +1,5 @@
 "use strict";
 
-var Globals = {
-  behaviours: {},
-  settings: {},
-  attach: function() {
-    for (var behaviour in Globals.behaviours) {
-      Globals.behaviours[behaviour]();
-    }
-  }
-};
-
 Global.behaviours.chosen = function () {
   $('select.chosen').chosen({
     allow_single_deselect: true
